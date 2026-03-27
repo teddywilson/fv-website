3ed157be790159e68ee6e9caecf29834a067db60
teddywilson <teddywilson@users.noreply.github.com>
2026-03-27 10:25:59 +0000

    deploy: b67d0d2d39b9cd15fb8dc9a29d91c69e8b397111

---
+++ _next/static/oeY8CwrrH4xG5Ow6JEIst/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-4c3765eb4dced244.js"],"/404":["static/chunks/pages/404-7b272f3b9377111d.js"],"/_error":["static/chunks/pages/_error-7a92967bea80186d.js"],sortedPages:["/","/404","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
